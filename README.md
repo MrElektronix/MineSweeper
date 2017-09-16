d43c6864eaacb7eaecbe89b8d2ca19a87e10b7cf
MrElektronix <milovanpelt@gmail.com>
2017-09-16 03:46:02 +0200

    Update
    - updatet readme
    - bomb is visible when you click on it
    - removed some minor things from the classes

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # MineSweeper
+
+Milo van Pelt - GD3A
+
+Info: 
+Try to clear the field while trying to avoid any mines.
+
+Play Minesweeper: http://www.milovanpelt.nl/minesweeper.html
--- main.js
+++ main.js
@@ -7,11 +7,13 @@ let offset, offsetX, offsetY;
 let mouseX, mouseY;
 let tile;
 let bomb;
+let randomBombX, randomBombY, calcBombX, calcBombY;
 
 let tiles = [];
 let blocks = [];
 let bombs = [];
 let bombCounter = 10;
+let gamestate = false;
 
 
 class Grid {
@@ -30,7 +32,6 @@ class Grid {
 				block.color = "grey";
 				block.filled = true;
 				
-				
 				blockX += blockOffset;
 				
 				tiles.push(tile);
@@ -44,6 +45,7 @@ class Grid {
 
 
 let init = ()=> {
+	gamestate = true;
 	canvas = document.getElementById("canvas");
 	context = canvas.getContext("2d");
 	
@@ -56,17 +58,23 @@ let init = ()=> {
 	update();
 	
 	for (let i = 0; i < bombCounter;){
-		let randomBombX = Math.floor(Math.random() * (canvas.width - blockOffset));
-		let randomBombY = Math.floor(Math.random() * (canvas.height - blockOffset));
+		randomBombX = Math.floor(Math.random() * (canvas.width - blockOffset));
+		randomBombY = Math.floor(Math.random() * (canvas.height - blockOffset));
 		
-		let calcBombX = calcBomb(randomBombX);
-		let calcBombY = calcBomb(randomBombY);
+		calcBombX = calcBomb(randomBombX);
+		calcBombY = calcBomb(randomBombY);
 		
 		if (calcBombX == 0 && calcBombY == 0) {
-			console.log("Bomb X: " + randomBombX +", " + "Bomb Y: " + randomBombY);
+			for(let i = 0; i < tiles.length; i ++) {
+				
+				if (randomBombX == tiles[i].x && randomBombY == tiles[i].y) {
+					bomb = new Rectangle(randomBombX, randomBombY, 40, 40);
+					bomb.filled = true;
+					bomb.color = "red";
+					bombs.push(bomb);
+				}
+			}
 			i++;
-		} else{
-			console.log("bomb not found");
 		}
 	}
 }
@@ -79,39 +87,46 @@ let calcBomb = (item)=> {
 let update = ()=> {
 	setOffset();
 	context.clearRect(0, 0, canvas.width, canvas.height);
+	for (let i = 0; i < bombs.length; i++) {
+		bombs[i].draw(context);
+	}
+	
 	for (let i = 0; i < blocks.length; i++) {
 		blocks[i].draw(context);
 	}
+	
 	for (let i = 0; i < tiles.length; i++) {
 		tiles[i].draw(context);
 	}
-	
-	
 
 	
+	if (gamestate) {
+		requestAnimationFrame(update);	
 	
-	requestAnimationFrame(update);
-	canvas.onmousedown = (e) => {
-		mouseX = e.clientX - offsetX;
-		mouseY = e.clientY - offsetY;
-		for (let i = 0; i < blocks.length; i++) {
-			if (mouseX >= blocks[i].x 
-				&& mouseX <= blocks[i].x + blocks[i].width 
-				&& mouseY >= blocks[i].y 
-				&& mouseY <= blocks[i].y + blocks[i].height){
-				//game.gameObjects[i].selected = true;
-				let index = blocks.indexOf(blocks[i]);
-				blocks.splice(index, 1);
-	
-				for (let i = 0; i < 10; i++) {
-					let randomIndex = Math.floor(Math.random() * blocks.length);
-					blocks.splice(randomIndex, 1);
+		canvas.onmousedown = (e) => {
+			mouseX = e.clientX - offsetX;
+			mouseY = e.clientY - offsetY;
+			for (let i = 0; i < blocks.length; i++) {
+				if (mouseX >= blocks[i].x 
+					&& mouseX <= blocks[i].x + blocks[i].width 
+					&& mouseY >= blocks[i].y 
+					&& mouseY <= blocks[i].y + blocks[i].height){
+					let index = blocks.indexOf(blocks[i]);
+					blocks.splice(index, 1);
+					
 				}
-				
+			}
+
+			for (let i = 0; i < bombs.length; i++) {
+				if (mouseX >= bombs[i].x 
+					&& mouseX <= bombs[i].x + bombs[i].width 
+					&& mouseY >= bombs[i].y 
+					&& mouseY <= bombs[i].y + bombs[i].height){
+					gamestate = false;
+				}
 			}
 		}
 	}
-
 }
 
 let setOffset = ()=> {
